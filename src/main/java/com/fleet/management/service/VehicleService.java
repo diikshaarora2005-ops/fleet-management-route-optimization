278e040f10c4643e2a54655742b39af630b4fd6d
diikshaarora2005-ops <diikshaarora2005@gmail.com>
2026-06-27 23:42:01 +0530

    feat: add Vehicle DTO conversion

--- src/main/java/com/fleet/management/service/VehicleService.java
+++ src/main/java/com/fleet/management/service/VehicleService.java
@@ -4,12 +4,24 @@ package com.fleet.management.service;
 import org.springframework.stereotype.Service;
 import com.fleet.management.repository.VehicleRepository;
 import com.fleet.management.entity.Vehicle;
+import com.fleet.management.dto.VehicleDto;
 
 @Service
 public class VehicleService {
 	private final VehicleRepository vehicleRepository;
 	public VehicleService(VehicleRepository vehicleRepository) {
 	    this.vehicleRepository = vehicleRepository;
+	}
+	public VehicleDto convertToDto(Vehicle vehicle) {
+
+	    VehicleDto dto = new VehicleDto();
+
+	    dto.setVehicleNumber(vehicle.getVehicleNumber());
+	    dto.setVehicleType(vehicle.getVehicleType());
+	    dto.setDriverName(vehicle.getDriverName());
+	    dto.setCapacity(vehicle.getCapacity());
+
+	    return dto;
 	}
 	public Vehicle saveVehicle(Vehicle vehicle) {
 	    return vehicleRepository.save(vehicle);
